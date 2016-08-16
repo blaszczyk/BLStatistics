1fd2bd3673f153aa721aa4c5565bf24b7f4d47aa
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2016-08-16 22:38:09 +0200

    Delete Old Package

--- src/bn/blaszczyk/blstatistics/filters/BiFilter.java
+++
@@ -1,6 +0,0 @@
-package bn.blaszczyk.blstatistics.filters;
-
-public interface BiFilter <T,U>
-{
-	public boolean check(T t,U u);
-}
--- src/bn/blaszczyk/blstatistics/filters/Filter.java
+++
@@ -1,6 +0,0 @@
-package bn.blaszczyk.blstatistics.filters;
-
-public interface Filter<T>
-{
-	public boolean check(T t);
-}
